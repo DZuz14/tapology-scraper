c24abe774ec7f8d3456ce5e929840a3df980b96f
Dan <danielzuzevich@gmail.com>
2020-02-17 15:34:41 -0500

    got rid of unnecessary logic in fighter-profile

--- scrapers/fighter-profile.js
+++ scrapers/fighter-profile.js
@@ -11,65 +11,24 @@ class FighterProfile {
    */
   async main() {
     await this.page.evaluate(() => {
-      let nickname = document.querySelector(
+      const hasText = (el, selector) => {
+        const _el = el.querySelector(selector);
+        return selected ? _el.innerText.trim() : "";
+      };
+
+      // const name =
+
+      const nickname = hasText(
         "#content > div.fighterUpcomingHeader > h4.preTitle.nickname"
       );
-      nickname = nickname ? nickname.innerText.trim() : "";
 
-      let record = document.querySelector(
+      const record = hasText(
         "#content > div.fighterUpcomingHeader > h1.prorecord"
       );
-      record = record ? record.innerText.trim() : "";
 
-      let age = document.querySelector("#stats > ul > li:nth-child(7)");
-      age = age ? age.innerText.trim() : "";
-
-      let heightReach = document.querySelector("#stats > ul > li:nth-child(9)");
-      heightReach = heightReach ? heightReach.innerText.trim() : "";
-
-      let country = document.querySelector("#stats > ul > li:nth-child(11)");
-      country = country ? country.innerText.trim() : "";
-
-      const pro = Array.from(
-        document.querySelectorAll(
-          "#fighterRecord > section:nth-child(3) .result"
-        )
-      ).map(fight => {
-        let mainInfo = fight.querySelector(".summary .lead");
-        mainInfo = mainInfo ? mainInfo.innerText.trim() : "";
-
-        let date = fight.querySelector(".date");
-        date = date ? date.innerText.trim() : "";
-
-        let event = fight.querySelector(".notes");
-        event = event ? event.innerText.trim() : "";
-
-        let record = fight.querySelector(
-          ".opponent > div.record > span:nth-child(1)"
-        );
-        record = record ? record.innerText.trim() : "";
-
-        let card = fight.querySelector(".detail.tall > div:nth-child(1)");
-        card = card ? card.innerText.trim() : "";
-
-        let timeFormat = fight.querySelector(".detail.tall > div:nth-child(2)");
-        timeFormat = timeFormat ? timeFormat.innerText.trim() : "";
-
-        let weightClass = fight.querySelector(
-          ".detail.tall > div:nth-child(3)"
-        );
-        weightClass = weightClass ? weightClass.innerText.trim() : "";
-
-        return {
-          mainInfo,
-          event,
-          date,
-          record,
-          card,
-          timeFormat,
-          weightClass
-        };
-      });
+      const age = hasText("#stats > ul > li:nth-child(7)");
+      const heightReach = hasText("#stats > ul > li:nth-child(9)");
+      const country = hasText("#stats > ul > li:nth-child(11)");
     });
   }
 }
--- scrapers/promotion.js
+++ scrapers/promotion.js
@@ -6,37 +6,62 @@ class Promotion {
     this.page = page;
   }
 
-  // #content > div.details.details_with_poster
-  // .left img
-
-  // .right li
-  // name, hq, acronyms, links
-
-  //   <div class="externalIconsHolder">
-  // <a href="https://www.facebook.com/ces.mma" onclick="ga('send', 'event', 'ExternalClick', 'Promotion_Facebook');" rel="nofollow" target="_blank">
-  // <div class="externalIcon">
-  // <img src="/assets/social/facebook-caaa8c8ad12aebc3b58ef8f696a1acff4222eeb72b01297b1d0ed9dfb2efd3c6.png" alt="Facebook">
-  // </div>
-  // </a>
-  // <a href="https://www.twitter.com/cesmma" onclick="ga('send', 'event', 'ExternalClick', 'Promotion_Twitter');" rel="nofollow" target="_blank">
-  // <div class="externalIcon">
-  // <img src="/assets/social/twitter-67a46008550d527a2d9dd557f90f1e0e2a093eb44767efb3c7393e5f7c809578.png" alt="Twitter">
-  // </div>
-  // </a>
-  // <a href="http://www.cesmma.com/" onclick="ga('send', 'event', 'ExternalClick', 'Promotion_Website');" rel="nofollow" target="_blank">
-  // <div class="externalIcon">
-  // <img alt="CES" src="https://images.tapology.com/logo_squares/36/icon_bw/CES-MMA-logo-sq.png?1567822305">
-  // </div>
-  // </a>
-  // <a href="https://instagram.com/cesmma" onclick="ga('send', 'event', 'ExternalClick', 'Promotion_Instagram');" rel="nofollow" target="_blank">
-  // <div class="externalIcon">
-  // <img src="/assets/social/instagram-0514a15687d72df706ccd753ee5fdca547fbddc29292b746c4c7944c64611ae1.png" alt="Instagram">
-  // </div>
-  // </a>
-  // </div>
-
+  /**
+   * @method main
+   */
   async main() {
-    //
+    const promotionInfo = await this.page.evaluate(() => {
+      const poster = document.querySelector(
+        "#content > div.details.details_with_poster .left img"
+      );
+
+      const info = Array.from(
+        document.querySelectorAll(
+          "#content > div.details.details_with_poster .right li"
+        )
+      ).reduce((acc, li) => {
+        const key = li
+          .querySelector("strong")
+          .innerText.trim()
+          .replace(":", "")
+          .toLowerCase();
+
+        let val;
+
+        if (key === "promotion links") {
+          const links = Array.from(
+            key.querySelectorAll(".externalIconsHolder a")
+          ).reduce((acc, link) => {
+            let linkType = link
+              .getAttribute("onclick")
+              .split(",")
+              .pop();
+
+            linkType = linkType
+              .slice(linkType.indexOf("_") + 1, linkType.lastIndexOf(`'`))
+              .toLowerCase();
+
+            return ["facebook", "twitter", "website", "instagram"].includes(
+              linkType
+            )
+              ? { ...acc, [linkType]: link.getAttribute("href") }
+              : acc;
+          }, {});
+        } else {
+          val = li.querySelector("span").innerText.trim();
+          val = key === "acronyms" ? val.split(",")[0] : val;
+        }
+
+        return { ...acc, [key]: val };
+      }, {});
+
+      return {
+        poster,
+        info
+      };
+    });
+
+    return promotionInfo;
   }
 }
 
