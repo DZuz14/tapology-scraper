b43a61620620582535419c629903ad97ed2e19cc
Dan <danielzuzevich@gmail.com>
2020-02-17 10:49:37 -0500

    navigation of pagination seems to be working

--- scripts/fight-center.js
+++ scripts/fight-center.js
@@ -1,43 +1,61 @@
 const puppeteer = require("puppeteer");
-const Event = require("../scrapers/event");
-const FighterProfile = require("../scrapers/fighter-profile");
+// const Event = require("../scrapers/event");
+// const FighterProfile = require("../scrapers/fighter-profile");
 
-let browser, page;
-
-(async () => {
-  browser = await puppeteer.launch({ args: ["--no-sandbox"] });
-  page = await browser.newPage();
-  await page.setDefaultNavigationTimeout(60000);
-})();
-
-(async () => {
-  const url =
-    "https://www.tapology.com/fightcenter?group=regional&schedule=results&sport=mma&region=2&page=";
-
-  let count = 1;
-
-  while (true) {
-    await page.goto(`${url}${count}`);
-    await page.waitForSelector(".fightcenterEvents");
-
-    const events = await page.evaluate(() =>
-      Array.from(
-        document.querySelectorAll("section .fcListing span.name")
-      ).map(event => event.getAttribute("href"))
-    );
-
-    if (!events.length) {
-      console.log("No events were found.");
-      break;
-    }
-
-    for (const event of events) {
-      console.log(event);
-
-      // Promotion
-      // Event
-    }
-
-    count++;
+/**
+ * FightCenter
+ */
+class FightCenter {
+  constructor() {
+    this.url =
+      "https://www.tapology.com/fightcenter?group=regional&schedule=results&sport=mma&region=2&page=";
+    this.count = 1;
   }
-})();
+
+  /**
+   * @method main
+   */
+  async main() {
+    this.browser = await puppeteer.launch({
+      args: ["--no-sandbox"],
+      headless: false
+    });
+
+    this.page = await this.browser.newPage();
+
+    await this.page.setDefaultNavigationTimeout(60000);
+    await this.visitEvents();
+    await this.browser.close();
+  }
+
+  /**
+   * @method visitEvents
+   */
+  async visitEvents() {
+    while (true) {
+      await this.page.goto(`${this.url}${this.count}`);
+      await this.page.waitForSelector(".fightcenterEvents");
+
+      const events = await this.page.evaluate(() =>
+        Array.from(
+          document.querySelectorAll("section.fcListing span.name a")
+        ).map(event => event.getAttribute("href"))
+      );
+
+      if (!events.length) {
+        console.log("No events were found.");
+        break;
+      }
+
+      for (const event of events) {
+        // Promotion
+        // Event Details
+        // Matches
+      }
+
+      this.count++;
+    }
+  }
+}
+
+new FightCenter().main();
